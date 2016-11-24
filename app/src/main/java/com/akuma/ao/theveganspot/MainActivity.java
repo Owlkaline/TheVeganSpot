bc53820c596b9ac67c6014c83ce18426505e7f4c
Lilith645 <lilith@inet-sys.com>
2016-11-24 13:41:02 +1000

    Adding Basic android project Changes committed: 	new file:   .idea/compiler.xml 	new file:   .idea/copyright/profiles_settings.xml 	new file:   .idea/gradle.xml 	new file:   .idea/misc.xml 	new file:   .idea/modules.xml 	new file:   .idea/runConfigurations.xml 	new file:   app/.gitignore 	new file:   app/build.gradle 	new file:   app/proguard-rules.pro 	new file:   app/src/androidTest/java/com/akuma/ao/theveganspot/ExampleInstrumentedTest.java 	new file:   app/src/main/AndroidManifest.xml 	new file:   app/src/main/java/com/akuma/ao/theveganspot/DisplayMessageActivity.java 	new file:   app/src/main/java/com/akuma/ao/theveganspot/MainActivity.java 	new file:   app/src/main/res/layout/activity_display_message.xml 	new file:   app/src/main/res/layout/activity_main.xml 	new file:   app/src/main/res/mipmap-hdpi/ic_launcher.png 	new file:   app/src/main/res/mipmap-mdpi/ic_launcher.png 	new file:   app/src/main/res/mipmap-xhdpi/ic_launcher.png 	new file:   app/src/main/res/mipmap-xxhdpi/ic_launcher.png 	new file:   app/src/main/res/mipmap-xxxhdpi/ic_launcher.png 	new file:   app/src/main/res/values-w820dp/dimens.xml 	new file:   app/src/main/res/values/colors.xml 	new file:   app/src/main/res/values/dimens.xml 	new file:   app/src/main/res/values/strings.xml 	new file:   app/src/main/res/values/styles.xml 	new file:   app/src/test/java/com/akuma/ao/theveganspot/ExampleUnitTest.java 	new file:   build.gradle 	new file:   gradle.properties 	new file:   gradle/wrapper/gradle-wrapper.jar 	new file:   gradle/wrapper/gradle-wrapper.properties 	new file:   gradlew 	new file:   gradlew.bat 	new file:   settings.gradle

---
+++ app/src/main/java/com/akuma/ao/theveganspot/MainActivity.java
@@ -0,0 +1,29 @@
+package com.akuma.ao.theveganspot;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static String EXTRA_MESSAGE="default_message";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        //So stuff
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(MainActivity.EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
